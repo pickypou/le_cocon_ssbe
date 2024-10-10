ed6c652f55121a8abe448a2b9f040cecaa7bc618
NouveauNom <ludo.spysschaert@orange.fr>
2024-10-10 15:42:57 +0200

    cofig firebase

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDcFwkYsp64HNZVgsY86TC7aE2_VM1SIqQ",
+  authDomain: "le-cocon-ssbe-6a4d0.firebaseapp.com",
+  projectId: "le-cocon-ssbe-6a4d0",
+  storageBucket: "le-cocon-ssbe-6a4d0.appspot.com",
+  messagingSenderId: "282482672251",
+  appId: "1:282482672251:web:77990508463fa9958058de"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
