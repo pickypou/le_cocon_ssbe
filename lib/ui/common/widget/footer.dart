eb55660b55ea64843cacb8fadfbf4120efe48af4
pickypou <spysschaert.ludo@orange.fr>
2025-05-16 14:13:39 +0200

    ajout d'un lien vers Ludovic spysschaert.fr

--- lib/ui/common/widget/footer.dart
+++ lib/ui/common/widget/footer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:le_cocon_ssbe/ui/common/widget/pdf_viewver.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../../theme.dart';
 import 'image_cliquable.dart';
 
@@ -34,7 +35,20 @@ class Footer extends StatelessWidget {
                   },
                   child: Text('Mentions_légales',
                       style: textStyleText(context)
-                          .copyWith(fontSize: size.width / 75)))
+                          .copyWith(fontSize: size.width / 75))),
+              TextButton(
+                onPressed: () async {
+                  final Uri url = Uri.parse('https://ludovicspysschaert.fr/');
+                  if (await canLaunchUrl(url)) {
+                    await launchUrl(url, mode: LaunchMode.externalApplication);
+                  } else {
+                    // Tu peux afficher une erreur ici
+                    throw 'Impossible d’ouvrir l’URL : $url';
+                  }
+                },
+                child:
+                Text('Céation Ludovic SPYSSCHAERT', style:textStyleText(context).copyWith(fontSize: 10, color: theme.colorScheme.error), textAlign: TextAlign.end,),
+              ),
             ],
           ),
         ),
